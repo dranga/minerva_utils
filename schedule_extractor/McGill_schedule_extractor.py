4af080358c3e913675351e25f081bfbfe5a8293a
dranga <daniel.ranga@gmail.com>
2014-02-22 12:56:37 -0500

    made usage clearer by using email example

--- schedule_extractor/McGill_schedule_extractor.py
+++ schedule_extractor/McGill_schedule_extractor.py
@@ -24,7 +24,7 @@ color = "lightgray"
 
 #argument parsing
 parser = argparse.ArgumentParser()
-parser.add_argument("email", help = "McGill email")
+parser.add_argument("john.doe0@mail.mcgill.ca", help = "McGill email")
 parser.add_argument("output_file", help = "File to write to (e.g. output.html)")
 parser.add_argument("weekof", help = "Schedule for specific week mm/dd/yyyy")
 parser.add_argument("-c","--color", help = "Highlight color (any css valid color, e.g. '-c red' or '-c #f00'). default: lightgray") 
